fea7c4cddc0665bf52b4b531068073b12c725216
czhyinshen <374056498@qq.com>
2016-08-05 17:35:36 +0800

    更新

---
+++ code/UploadWebPJ/src/main/java/com/unicomWeb/model/ResponseMeta.java
@@ -0,0 +1,38 @@
+package com.unicomWeb.model;
+
+/**
+ * Created by York on 2016/8/5.
+ */
+
+public class ResponseMeta {
+
+    private String statusCode;
+
+    private String message;
+
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
